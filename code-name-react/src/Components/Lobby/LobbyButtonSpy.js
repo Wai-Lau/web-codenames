2b63a0dd20d85133bdfc566b66e9d1358513af81
fredriclam <fredriclam@live.com>
2018-02-04 12:21:49 -0500

    Minor changes to spy heads on Lobby

--- code-name-react/src/Components/Lobby/LobbyButtonSpy.js
+++ code-name-react/src/Components/Lobby/LobbyButtonSpy.js
@@ -6,15 +6,29 @@ class LobbyButtonSpy extends Component {
   render() {
     return (
       <center>
-      <div className="col-sm-6">
-        <div className="SquareButton" id="spymaster1"></div>
-        <div className="ThinButton" id="spy1"></div>
+
+      <div className="col-xs-6" id="spyset">
+        <div className="size-capped">
+          <div className="SquareButton" id="spymaster1">
+          </div>
+        </div>
+        <div className="size-capped">
+          <div className="ThinButton sized-capped" id="spy1">
+          </div>
+        </div>
       </div>
 
-      <div className="col-sm-6">
-        <div className="SquareButton" id="spymaster2"></div>
-        <div className="ThinButton" id="spy2"></div>
+      <div className="col-xs-6" id="spyset">
+        <div className="size-capped">
+          <div className="SquareButton" id="spymaster2">
+          </div>
+        </div>
+        <div className="size-capped">
+          <div className="ThinButton" id="spy2">
+          </div>
+        </div>
       </div>
+
       </center>
     );
   }
